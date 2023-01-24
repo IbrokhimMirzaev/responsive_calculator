37d48b56f60b79926ee2979d991acf0d8ba1cd4b
IbrokhimMirzaev <mirzaevibrokhim@gmail.com>
2023-01-24 12:25:17 +0500

    warnings are reduced

--- app/src/main/java/com/example/responsivecalculator/MainActivity.kt
+++ app/src/main/java/com/example/responsivecalculator/MainActivity.kt
@@ -2,10 +2,64 @@ package com.example.responsivecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initUI()
+        one.setOnClickListener(this)
+        two.setOnClickListener(this)
+        three.setOnClickListener(this)
+        four.setOnClickListener(this)
+        five.setOnClickListener(this)
+        six.setOnClickListener(this)
+        seven.setOnClickListener(this)
+        eight.setOnClickListener(this)
+        nine.setOnClickListener(this)
+        zero.setOnClickListener(this)
+    }
+
+    private lateinit var one: Button
+    private lateinit var two: Button
+    private lateinit var three: Button
+    private lateinit var four: Button
+    private lateinit var five: Button
+    private lateinit var six: Button
+    private lateinit var seven: Button
+    private lateinit var eight: Button
+    private lateinit var nine: Button
+    private lateinit var zero: Button
+
+    private lateinit var text1: TextView
+    private lateinit var text2: TextView
+
+    private fun initUI() {
+        one = findViewById(R.id.one)
+        two = findViewById(R.id.two)
+        three = findViewById(R.id.three)
+        four = findViewById(R.id.four)
+        five = findViewById(R.id.five)
+        six = findViewById(R.id.six)
+        seven = findViewById(R.id.seven)
+        eight = findViewById(R.id.eight)
+        nine = findViewById(R.id.nine)
+        zero = findViewById(R.id.zero)
+
+        text1 = findViewById(R.id.text1)
+        text2 = findViewById(R.id.text2)
+    }
+
+    override fun onClick(p0: View?) {
+        val btn = findViewById<Button>(p0!!.id)
+        if (text1.text != "0"){
+            text1.text = text1.text.toString() + btn.text
+        }
+        else {
+            text1.text = btn.text
+        }
     }
 }
