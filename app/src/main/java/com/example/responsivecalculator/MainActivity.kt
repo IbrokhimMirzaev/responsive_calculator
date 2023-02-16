63740f06435cf876e222c3c0b325207c78c4560e
Khamroev001 <sattorkhamroev@gmail.com>
2023-02-16 08:40:36 +0500

    qoshe

--- app/src/main/java/com/example/responsivecalculator/MainActivity.kt
+++ app/src/main/java/com/example/responsivecalculator/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+
     private lateinit var one: Button
     private lateinit var two: Button
     private lateinit var three: Button
